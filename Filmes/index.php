1f7b854d16375bfc4151ea3bd1cc9c776c696765
Fernando Carosi Lourenço <zippetanimations@gmail.com>
2025-06-06 22:18:02 -0300

    ,

--- Filmes/index.php
+++ Filmes/index.php
@@ -3,27 +3,29 @@ include "cabecalho.php";
 include "banner.php";
 ?>
 
-    <h2>Melhores avaliações</h2>
-    <div class="lista-filmes">
-        <div class="card-filme">
-            <img src="img/Steve.jfif">
+ <div class="container">
+    <h2 class="display-5">Melhores avaliações</h2>
+    <div class="row">
+        <div class="col">
+            <img src="img/Steve.jfif" class="img-fluid">
             <h3><a href="umfilme.php">Um filme minecraft</a></h3>
             <span>⭐ 10/10</span>
         </div>
-        <div class="card-filme">
+        <div class="col">
             <img src="img/EDOM.jfif">
             <h3><a href="umfilme.php"> Enigma De Outro Mundo</a></h3>
             <span>⭐ 10/10</span>
         </div>
-        <div class="card-filme">
+        <div class="col">
             <img src="img/Alien.webp">
             <h3><a href="umfilme.php">Alien : O Oitavo Passageiro</a></h3>
             <span>⭐ 10/10</span>
         </div>
-        <div class="card-filme">
+        <div class="col">
             <img src="img/JurassicP.jfif">
             <h3>Jurassic Park</h3>
             <span>⭐ 10/10</span>
         </div>
+    </div>
     </div>
     <?php include "rodape.php"; ?>
